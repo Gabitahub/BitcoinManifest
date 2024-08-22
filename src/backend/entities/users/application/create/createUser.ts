1af505cf891afb820a4087b79c453cf5b732564b
Gabriel Drach <gabitahub@myregulardomain.com>
2024-08-22 10:19:25 -0300

    Update texto invites

--- src/backend/entities/users/application/create/createUser.ts
+++ src/backend/entities/users/application/create/createUser.ts
@@ -45,3 +45,20 @@ export function createUser(
         }
     };
 }
+
+/* Solo se usa una vez para el primer user.
+export function createUser(userRepository: UserRepository): (user: UserDataCreate, invitation: string) => Promise<User | null> {
+    return async (user: UserDataCreate, invitation: string): Promise<User | null> => {
+        try {
+            const userInvitationTokens = Array(5).fill(null).map(() => createToken());
+            const hqImage = user.image ? removeLasOcurrenceWordFromString(user.image, "_normal") : "";
+            const newUser = await userRepository.create({ ...user, image: hqImage }, userInvitationTokens);
+            return newUser;
+        }
+        catch (error) {
+            console.log("error", error); 
+            return null;
+        }
+    };
+}
+*/
